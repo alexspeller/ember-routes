e0175bee8f036a8fcb2f817b40aa8c0e7fb191e4
Alex Speller <alex@alexspeller.com>
2015-10-28 15:20:01 +0000

    Global toolbar

--- app/application/route.js
+++ app/application/route.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('email');
+  },
+
+  renderTemplate() {
+    this._super(...arguments);
+    this.render("application/toolbar", {
+      into:   'application',
+      outlet: 'toolbar'
+    });
   }
+
 });
