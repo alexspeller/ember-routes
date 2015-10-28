ca7b196bb9d6b90f9f1eb4f6708859ca01097fed
Alex Speller <alex@alexspeller.com>
2015-10-28 14:41:35 +0000

    Loading and error substates

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -10,7 +10,7 @@ export default function() {
   */
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-   this.timing = 100;      // delay for each request, automatically set to 0 during testing
+   this.timing = 1000;      // delay for each request, automatically set to 0 during testing
 
   /*
     Route shorthand cheatsheet
