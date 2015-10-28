d78430baf48e1e3af81698f2380b338072a99a00
Alex Speller <alex@alexspeller.com>
2015-10-28 15:01:40 +0000

    Exploiting index route for editing emails

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('mailbox', {path: "/:mailbox_id"}, function() {
-    this.route('email', {path: "/emails/:email_id"});
+    this.route('email', {path: "/emails/:email_id"}, function() {
+      this.route('edit')
+    });
   });
 });
 
